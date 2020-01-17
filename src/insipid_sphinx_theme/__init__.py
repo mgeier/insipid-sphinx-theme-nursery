4981cc993b0da373cf215ab0d3017cf2f1cccbde
Matthias Geier <Matthias.Geier@gmail.com>
2020-01-17 10:21:13 +0100

    Move theme files to "insipid" directory

--- src/insipid_sphinx_theme/__init__.py
+++ src/insipid_sphinx_theme/__init__.py
@@ -3,4 +3,6 @@ from os import path
 
 def setup(app):
     """Sphinx extension entry point."""
-    app.add_html_theme('insipid', path.abspath(path.dirname(__file__)))
+    app.add_html_theme(
+        'insipid',
+        path.abspath(path.join(path.dirname(__file__), 'insipid')))

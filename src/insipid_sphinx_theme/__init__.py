a5d6390f5b450f8e4f190500297040481b148a4a
Matthias Geier <Matthias.Geier@gmail.com>
2020-01-17 11:39:26 +0100

    Use require_sphinx()

--- src/insipid_sphinx_theme/__init__.py
+++ src/insipid_sphinx_theme/__init__.py
@@ -5,6 +5,7 @@ __version__ = '0.0.0'
 
 def setup(app):
     """Sphinx extension entry point."""
+    app.require_sphinx('1.6')  # For add_html_theme()
     app.add_html_theme(
         'insipid',
         path.abspath(path.join(path.dirname(__file__), 'insipid')))

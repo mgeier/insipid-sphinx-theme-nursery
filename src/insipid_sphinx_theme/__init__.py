3ea490b0ef3a39431bfc68288387a0fe9db5c356
Matthias Geier <Matthias.Geier@gmail.com>
2020-04-20 19:31:00 +0200

    Use pathlib in setup()

--- src/insipid_sphinx_theme/__init__.py
+++ src/insipid_sphinx_theme/__init__.py
@@ -1,5 +1,5 @@
 """An insipid Sphinx theme."""
-from os import path
+from pathlib import Path
 
 __version__ = '0.0.0'
 
@@ -7,9 +7,7 @@ __version__ = '0.0.0'
 def setup(app):
     """Sphinx extension entry point."""
     app.require_sphinx('1.6')  # For add_html_theme()
-    app.add_html_theme(
-        'insipid',
-        path.abspath(path.join(path.dirname(__file__), 'insipid')))
+    app.add_html_theme('insipid', Path(__file__).resolve().parent / 'insipid')
     return {
         'version': __version__,
         'parallel_read_safe': True,

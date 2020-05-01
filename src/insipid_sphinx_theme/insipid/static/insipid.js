619d607754249cdc8492468350a3109b46105c62
Matthias Geier <Matthias.Geier@gmail.com>
2020-05-01 16:51:02 +0200

    focus after button press, jQuery variable names

--- src/insipid_sphinx_theme/insipid/static/insipid.js
+++ src/insipid_sphinx_theme/insipid/static/insipid.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     'use strict';
 
-    var body = $(document.body);
+    var $body = $(document.body);
 
-    body.removeClass('loading');
+    $body.removeClass('loading');
 
     var scroll_timeout;
     var ignore_scroll = true;
@@ -14,14 +14,14 @@ $(document).ready(function () {
         return function () {
             window.clearTimeout(scroll_timeout);
             if (scroller.scrollTop === 0) {
-                body.removeClass('topbar-folded');
+                $body.removeClass('topbar-folded');
             } else if (ignore_scroll) {
                 // We ignore single jumps
                 ignore_scroll = false;
             } else if (scroller.scrollTop - previous > 0) {
-                body.addClass('topbar-folded');
+                $body.addClass('topbar-folded');
             } else {
-                body.removeClass('topbar-folded');
+                $body.removeClass('topbar-folded');
             }
             previous = Math.max(scroller.scrollTop, 0);
             scroll_timeout = setTimeout(function() {
@@ -42,37 +42,37 @@ $(document).ready(function () {
 
     $(document).on('scroll', function () {
         if (window.pageYOffset >= div_body.offsetTop + first_section.offsetTop) {
-            body.addClass('scrolled');
+            $body.addClass('scrolled');
         } else {
-            body.removeClass('scrolled');
+            $body.removeClass('scrolled');
         }
     });
 
     $(document).scroll();
 
     // show search
-    var form = $('#searchform');
-    var icon = $('#search-toggle');
-    icon.on('click', function () {
-        if (form.hasClass('hidden')) {
-            form.removeClass('hidden');
-            icon.attr('aria-expanded', 'true');
+    var $form = $('#searchform');
+    var $icon = $('#search-toggle');
+    $icon.on('click', function () {
+        if ($form.hasClass('hidden')) {
+            $form.removeClass('hidden');
+            $icon.attr('aria-expanded', 'true');
             $('#searchbar').focus();
         } else {
-            form.addClass('hidden');
-            icon.attr('aria-expanded', 'false');
+            $form.addClass('hidden');
+            $icon.attr('aria-expanded', 'false');
         }
     });
 
     if (document.fullscreenEnabled) {
-        $('#fullscreen').click(function() {
+        var $fullscreen = $('#fullscreen');
+        $fullscreen.click(function() {
             if (!document.fullscreenElement) {
                 document.documentElement.requestFullscreen();
-                document.scrollingElement.focus();
             } else {
                 document.exitFullscreen();
-                document.scrollingElement.focus();
             }
+            $fullscreen.blur();
         });
     } else {
         $('#fullscreen').remove();

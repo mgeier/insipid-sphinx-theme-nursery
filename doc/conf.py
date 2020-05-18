26000d62df289feedec2fa73c82c2dee226c2561
Matthias Geier <Matthias.Geier@gmail.com>
2020-05-18 19:49:46 +0200

    Enable readthedocs-sphinx-search

--- doc/conf.py
+++ doc/conf.py
@@ -110,6 +110,7 @@ html_secnumber_suffix = '\N{FIGURE SPACE}'
 # -- Sphinx extensions --------------------------------------------------------
 
 extensions = [
+    'sphinx_search.extension',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
